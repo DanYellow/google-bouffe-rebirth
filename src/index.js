605612a1005d493174b5ec564ec11c7a55cafc2c
DanYellow <jeanlouis.danielo@sfr.fr>
2017-05-08 19:31:11 +0200

    add react-ally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import a11y from 'react-a11y';
+
+if (process.env.NODE_ENV === 'development') a11y(React);
 
 import reducer from './reducers';
 
